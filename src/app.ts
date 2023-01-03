cc9664c9b5a848bd2056de85931c716864e0425d
Han <91880235+Mr-Hanbean@users.noreply.github.com>
2023-01-03 22:40:18 +0900

    Feat: App, PageComponent 구현

--- src/app.ts
+++ src/app.ts
@@ -1,50 +1,12 @@
-{
-    interface BlockMaker {
-        block: Block;
-        addVideo(video: Video): void;
-        addPicture(picture: Picture): void;
-        addNote(note: Note): void;
-        addTodo(todo: Todo): void;
-        deleteOne<T>(): void;
+import { PageComponent } from "./components/page";
+
+
+class App {
+    private readonly page: PageComponent;
+    constructor(appRoot: HTMLElement) {
+        this.page = new PageComponent();
+        this.page.attachTo(appRoot);
     }
-
-    type Block = any[];
-    
-    type Video = {
-        title: string;
-        url: string;
-    }
-
-    type Picture = Video
-
-    type Note = {
-        title: string;
-        body: string;
-    }
-
-    type Todo = Note
-
-    class BlockMakerImpl implements BlockMaker {
-        block: Block = []
-        constructor() {
-        }
-
-        addVideo(video: Video) {
-            document.querySelector
-            this.block.push(video);
-        }   
-        addPicture(picture: Picture) {
-            this.block.push(picture);
-        }
-        addNote(note: Note) {
-            this.block.push(note);
-        }
-        addTodo(todo: Todo) {
-            this.block.push(todo);
-        }
-        deleteOne<T>() {
-        }
-    }
-
-    const blockMaker = new BlockMakerImpl();
 }
+
+new App(document.querySelector('.document')! as HTMLElement);
