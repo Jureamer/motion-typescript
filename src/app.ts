e29229ad4c1b911f617ad1db464ec4e9ea6b1d8d
Han <91880235+Mr-Hanbean@users.noreply.github.com>
2023-01-30 22:45:12 +0900

    feat: 드래그 기능 구현 완료

--- src/app.ts
+++ src/app.ts
@@ -1,55 +1,55 @@
-import { InputDialog, MediaData, TextData } from './components/dialog/dialog.js'
-import { MediaSectionInput } from './components/dialog/input/media-input.js'
-import { TextSectionInput } from './components/dialog/input/text-input.js'
-import { Component } from './components/page/component.js'
-import { ImageComponent } from './components/page/item/image.js'
-import { NoteComponent } from './components/page/item/note.js'
-import { TodoComponent } from './components/page/item/todo.js'
-import { VideoComponent } from './components/page/item/video.js'
-import { Composable, PageComponent } from './components/page/page.js'
+import { InputDialog, MediaData, TextData } from './components/dialog/dialog.js';
+import { MediaSectionInput } from './components/dialog/input/media-input.js';
+import { TextSectionInput } from './components/dialog/input/text-input.js';
+import { Component } from './components/page/component.js';
+import { ImageComponent } from './components/page/item/image.js';
+import { NoteComponent } from './components/page/item/note.js';
+import { TodoComponent } from './components/page/item/todo.js';
+import { VideoComponent } from './components/page/item/video.js';
+import { Composable, PageComponent, PageItemComponent } from './components/page/page.js';
 
 type InputComponentConstructor<T = (MediaData | TextData) & Component> = {
-    new (): T
-}
+    new (): T;
+};
 class App {
-    private readonly page: Component & Composable
+    private readonly page: Component & Composable;
     constructor(appRoot: HTMLElement, private dialogRoot: HTMLElement) {
-        this.page = new PageComponent()
-        this.page.attachTo(appRoot)
+        this.page = new PageComponent(PageItemComponent);
+        this.page.attachTo(appRoot);
 
         this.bindElementToDialog<MediaSectionInput>(
             '#new-image',
             MediaSectionInput,
             (input: MediaSectionInput) => new ImageComponent(input.title, input.url),
-        )
+        );
 
         this.bindElementToDialog<MediaSectionInput>(
             '#new-video',
             MediaSectionInput,
             (input: MediaSectionInput) => new VideoComponent(input.title, input.url),
-        )
+        );
 
         this.bindElementToDialog<TextSectionInput>(
             '#new-note',
             TextSectionInput,
             (input: TextSectionInput) => new NoteComponent(input.title, input.body),
-        )
+        );
 
         this.bindElementToDialog<TextSectionInput>(
             '#new-todo',
             TextSectionInput,
             (input: TextSectionInput) => new TodoComponent(input.title, input.body),
-        )
+        );
 
         // Demo Content
-        this.page.addChild(new ImageComponent('Image Title', 'https://picsum.photos/600/300'))
-        this.page.addChild(new VideoComponent('Video Title', 'https://www.youtube.com/watch?v=08BbExqhcWw'))
-        this.page.addChild(new NoteComponent('Note Title', 'This is a note.'))
-        this.page.addChild(new TodoComponent('Todo Title', 'This is a todo.'))
-        this.page.addChild(new ImageComponent('Image Title', 'https://picsum.photos/600/300'))
-        this.page.addChild(new VideoComponent('Video Title', 'https://www.youtube.com/watch?v=08BbExqhcWw'))
-        this.page.addChild(new NoteComponent('Note Title', 'This is a note.'))
-        this.page.addChild(new TodoComponent('Todo Title', 'This is a todo.'))
+        this.page.addChild(new ImageComponent('Image Title', 'https://picsum.photos/600/300'));
+        this.page.addChild(new VideoComponent('Video Title', 'https://www.youtube.com/watch?v=08BbExqhcWw'));
+        this.page.addChild(new NoteComponent('Note Title', 'This is a note.'));
+        this.page.addChild(new TodoComponent('Todo Title', 'This is a todo.'));
+        this.page.addChild(new ImageComponent('Image Title', 'https://picsum.photos/600/300'));
+        this.page.addChild(new VideoComponent('Video Title', 'https://www.youtube.com/watch?v=08BbExqhcWw'));
+        this.page.addChild(new NoteComponent('Note Title', 'This is a note.'));
+        this.page.addChild(new TodoComponent('Todo Title', 'This is a todo.'));
     }
 
     private bindElementToDialog<T extends (MediaData | TextData) & Component>(
@@ -57,24 +57,24 @@ class App {
         inputComponent: InputComponentConstructor<T>,
         makeSection: (input: T) => Component,
     ) {
-        const element = document.querySelector(selector)! as HTMLElement
+        const element = document.querySelector(selector)! as HTMLElement;
         element.addEventListener('click', () => {
-            const dialog = new InputDialog()
-            const input = new inputComponent()
-            dialog.addChild(input)
-            dialog.attachTo(this.dialogRoot)
+            const dialog = new InputDialog();
+            const input = new inputComponent();
+            dialog.addChild(input);
+            dialog.attachTo(this.dialogRoot);
 
             dialog.setOnCloseListener(() => {
-                dialog.removeFrom(this.dialogRoot)
-            })
+                dialog.removeFrom(this.dialogRoot);
+            });
 
             dialog.setOnSubmitListener(() => {
-                const section = makeSection(input)
-                this.page.addChild(section)
-                dialog.removeFrom(this.dialogRoot)
-            })
-        })
+                const section = makeSection(input);
+                this.page.addChild(section);
+                dialog.removeFrom(this.dialogRoot);
+            });
+        });
     }
 }
 
-new App(document.querySelector('.document')! as HTMLElement, document.body)
+new App(document.querySelector('.document')! as HTMLElement, document.body);
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,12 +1,27 @@
-import { Component, BaseComponent } from './component.js'
+import { Component, BaseComponent } from './component.js';
 
 export interface Composable {
-    addChild(child: Component): void
+    addChild(child: Component): void;
 }
 
-type OnCloseListener = () => void
-export class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
-    private closeListener?: OnCloseListener
+type DragState = 'start' | 'end' | 'enter' | 'leave';
+type OnCloseListener = () => void;
+type OnDragStateListener<T extends Component> = (target: T, state: DragState) => void;
+
+type SectionContainerConstructor = {
+    new (): SectionContainer;
+};
+interface SectionContainer extends Component, Composable {
+    setOnCloseListener(listener: OnCloseListener): void;
+    setOnDragStateListener(listener: OnDragStateListener<SectionContainer>): void;
+    muteChildren(state: 'mute' | 'unmute'): void;
+    getBoundingRect(): DOMRect;
+    onDropped(): void;
+}
+
+export class PageItemComponent extends BaseComponent<HTMLElement> implements SectionContainer {
+    private closeListener?: OnCloseListener;
+    private dragStateListener?: OnDragStateListener<PageItemComponent>;
 
     constructor() {
         super(`<li draggable="true" class="page-item">
@@ -14,64 +29,150 @@ export class PageItemComponent extends BaseComponent<HTMLElement> implements Com
                     <div class="page-item__controls">
                         <button class="close" onclick="() => this.closeChild">&times;</button>
                     </div>
-                </li>`)
-        console.log(this.element)
-        const closeButton = this.element.querySelector('.close')
+                </li>`);
+        const closeButton = this.element.querySelector('.close');
         closeButton?.addEventListener('click', () => {
-            this.closeListener && this.closeListener()
-        })
+            this.closeListener && this.closeListener();
+        });
 
         this.element.addEventListener('dragstart', (event: DragEvent) => {
-            this.onDragStart(event)
-        })
+            this.onDragStart(event);
+        });
         this.element.addEventListener('dragend', (event: DragEvent) => {
-            this.onDragEnd(event)
-        })
+            this.onDragEnd(event);
+        });
+
+        this.element.addEventListener('dragenter', (event: DragEvent) => {
+            this.onDragEnter(event);
+        });
+        this.element.addEventListener('dragleave', (event: DragEvent) => {
+            this.onDragLeave(event);
+        });
     }
 
-    onDragStart(event: DragEvent) {
-        console.log('dragstart', event)
+    onDragStart(_: DragEvent) {
+        this.notifyingDragObservers('start');
+        this.element.classList.add('lifted');
     }
-    onDragEnd(event: DragEvent) {
-        console.log('dragend', event)
+    onDragEnd(_: DragEvent) {
+        this.notifyingDragObservers('end');
+        this.element.classList.remove('lifted');
     }
 
+    onDragEnter(_: DragEvent) {
+        this.notifyingDragObservers('enter');
+        this.element.classList.add('drop-area');
+    }
+
+    onDragLeave(_: DragEvent) {
+        this.notifyingDragObservers('leave');
+        this.element.classList.remove('drop-area');
+    }
+
+    onDropped() {
+        this.element.classList.remove('drop-area');
+    }
+    notifyingDragObservers(state: DragState) {
+        this.dragStateListener && this.dragStateListener(this, state);
+    }
     addChild(child: Component) {
-        const container = this.element.querySelector('.page-item__body')! as HTMLElement
-        child.attachTo(container)
+        const container = this.element.querySelector('.page-item__body')! as HTMLElement;
+        child.attachTo(container);
     }
 
     setOnCloseListener(listener: OnCloseListener) {
-        this.closeListener = listener
+        this.closeListener = listener;
+    }
+
+    setOnDragStateListener(listener: OnDragStateListener<PageItemComponent>) {
+        this.dragStateListener = listener;
+    }
+
+    muteChildren(state: 'mute' | 'unmute'): void {
+        if (state === 'mute') {
+            this.element.classList.add('mute-children');
+        } else {
+            this.element.classList.remove('mute-children');
+        }
+    }
+
+    getBoundingRect(): DOMRect {
+        return this.element.getBoundingClientRect();
     }
 }
 
 export class PageComponent extends BaseComponent<HTMLElement> implements Composable {
-    constructor() {
-        super('<ul class="page"></ul>')
+    private children = new Set<SectionContainer>();
+    private dropTarget?: SectionContainer;
+    private dragTarget?: SectionContainer;
+    constructor(private pageItemConstructor: SectionContainerConstructor) {
+        super('<ul class="page"></ul>');
 
         this.element.addEventListener('dragover', (event: DragEvent) => {
-            this.onDragOver(event)
-        })
+            this.onDragOver(event);
+        });
         this.element.addEventListener('drop', (event: DragEvent) => {
-            this.onDrop(event)
-        })
+            this.onDrop(event);
+        });
     }
+
     onDragOver(event: DragEvent) {
-        event.preventDefault()
-        console.log('dragover', event)
+        event.preventDefault();
+        // console.log('dragover', event)
     }
     onDrop(event: DragEvent) {
-        event.preventDefault()
-        console.log('drop', event)
+        event.preventDefault();
+        // console.log('drop', event)
+
+        if (!this.dropTarget) {
+            return;
+        }
+
+        if (this.dragTarget && this.dropTarget !== this.dragTarget) {
+            const dropY = event.clientY;
+            const srcElement = this.dragTarget.getBoundingRect();
+
+            this.dragTarget.removeFrom(this.element);
+            this.dropTarget.attach(this.dragTarget, dropY < srcElement.y ? 'beforebegin' : 'afterend');
+        }
+
+        this.dropTarget.onDropped();
     }
 
     addChild(section: Component) {
-        const item = new PageItemComponent()
-        item.addChild(section)
-        item.attachTo(this.element, 'beforeend')
+        const item = new this.pageItemConstructor();
+        item.addChild(section);
+        item.attachTo(this.element, 'beforeend');
         item.setOnCloseListener(() => {
-            item.removeFrom(this.element)
-        })
+            item.removeFrom(this.element);
+            this.children.delete(item);
+        });
+        this.children.add(item);
+        item.setOnDragStateListener((target: SectionContainer, state: DragState) => {
+            switch (state) {
+                case 'start':
+                    this.dragTarget = target;
+                    this.updateSections('mute');
+                    break;
+                case 'end':
+                    this.dragTarget = undefined;
+                    this.updateSections('unmute');
+                    break;
+                case 'enter':
+                    this.dropTarget = target;
+                    break;
+                case 'leave':
+                    this.dropTarget = undefined;
+                    break;
+                default:
+                    throw new Error(`unsupported state: ${state}`);
+            }
+            console.log(target, state);
+        });
+    }
+    private updateSections(state: 'mute' | 'unmute') {
+        this.children.forEach((section: SectionContainer) => {
+            section.muteChildren(state);
+        });
     }
 }
